8bbfb9ac3d6189bb5257701ec8d0bdf0892d0cdc
robertjankowski <robjanw11@wp.pl>
2019-03-23 11:04:56 +0100

    config cmake

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
-#include "tests.h"
+#include "include/tests.h"
 
 int main()
 {
-    // testRandom();
-    // testMatrixMultiplication();
-    // testTranspose();
-    // testRandomShuffle();
-    // testMatrixConcatSplit();
     testNeuralNet();
 
     // TODO: add parameters to script (in `run.sh`), e.g. to select learning rate and epoches
